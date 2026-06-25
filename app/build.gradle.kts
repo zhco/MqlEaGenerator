de5b5e3fbf9e888f913c5480e6e41fbbfa8109ed
zhco <luycheung@qq.com>
2026-06-25 18:37:13 +0800

    Add kotlinx.serialization dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("androidx.compose.material:material-icons-extended")
 
     // Networking (OkHttp)
